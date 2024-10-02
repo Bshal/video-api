06fe3141f142e7117f2b05cc827c12c8c1ff04ff
bshal <bishalchettri408@gmail.com>
2024-10-02 13:01:32 +0530

    feat: add video link share functionality

--- src/controllers/share.controller.js
+++ src/controllers/share.controller.js
@@ -1,5 +1,55 @@
-const catchAsync = require('../utils/catchAsync')
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
+const httpStatus = require('http-status')
 
-exports.createShareLink = catchAsync(async (req, res) => {})
+const db = require('../models')
+const catchAsync = require('../utils/catchAsync')
+const sendResponse = require('../utils/dataResponse')
+
+exports.createShareLink = catchAsync(async (req, res) => {
+	const { expiryTime } = req.body
+
+	const videoId = parseInt(req.params.videoId, 10)
+	if (isNaN(videoId)) {
+		return res
+			.status(httpStatus.BAD_REQUEST)
+			.json(
+				sendResponse(httpStatus.BAD_REQUEST, null, 'Invalid video ID format')
+			)
+	}
+
+	if (!expiryTime || typeof expiryTime !== 'number' || expiryTime <= 0) {
+		return res
+			.status(httpStatus.BAD_REQUEST)
+			.json(sendResponse(httpStatus.BAD_REQUEST, null, 'Invalid expiry time'))
+	}
+
+	const video = await db.Video.findByPk(videoId)
+	if (!video) {
+		return res
+			.status(httpStatus.NOT_FOUND)
+			.json(sendResponse(httpStatus.NOT_FOUND, null, 'Video not found'))
+	}
+
+	const shareToken = uuidv4()
+	const expiresAt = new Date(Date.now() + expiryTime * 60000)
+
+	await db.SharedLink.create({
+		videoId,
+		shareToken,
+		expiresAt,
+	})
+
+	res.status(httpStatus.CREATED).json(
+		sendResponse(
+			httpStatus.NOT_FOUND,
+			{
+				link: `${req.protocol}://${req.get('host')}/share/${shareToken}`,
+				expiresAt,
+			},
+			'Share link created successfully!'
+		)
+	)
+})
 
 exports.accessSharedVideo = catchAsync(async (req, res) => {})
